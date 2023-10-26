d92158a9a38facc4c4dcd2d649e1808dafe49c4c
Pablo <dalbem@gmail.com>
2023-10-26 18:50:11 -0300

    Acesso ao BD

--- 6. Acesso a BD/AcessoSQLite/src/Main.java
+++ 6. Acesso a BD/AcessoSQLite/src/Main.java
@@ -6,9 +6,9 @@ public class Main {
          //ATENÇÃO: Se você executar este código mais de uma vez
         // sem alterar a chave-primária do curso, ocorrerá um erro.
          Curso c1 = new Curso(100,"Java");
-       //  c1.salvar();
+         c1.salvar();
          c1.setNome("Java Avançado");
-     //    c1.atualizar();
+         c1.atualizar();
 
 
 
@@ -17,7 +17,7 @@ public class Main {
          //ATENÇÃO: Se você executar este código mais de uma vez
         // sem alterar a chave-primária do curso, ocorrerá um erro.
          Curso c2 = new Curso(200,"Android");
-       //  c2.salvar();
+         c2.salvar();
 
         List<Curso> listaCursos;
         listaCursos=Curso.buscarTodos();
@@ -27,9 +27,9 @@ public class Main {
         //ATENÇÃO: Se você executar este código mais de uma vez
         // sem alterar a chave-primária do aluno, ocorrerá um erro.
         Aluno a1 = new Aluno(1,"12345","Maria",c1);
-     //   a1.salvar();
+        a1.salvar();
         a1.setProntuario("123456");
-      //  a1.atualizar();
+        a1.atualizar();
         List<Aluno> listaAlunos;
         listaAlunos=Aluno.buscarTodos();
         for  (Aluno aluno : listaAlunos)
